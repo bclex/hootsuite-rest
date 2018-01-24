2f7b1f5643d24ebed5ef5d8e3acf38b8e40261a1
Sky Morey <smorey@degdigital.com>
2018-01-24 15:08:00 -0600

    adding .net

---
+++ src.net/Hootsuite.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hootsuite.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
