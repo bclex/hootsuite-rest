c334f7658688338593590ac5aea818be44fbfc5f
Sky Morey <smorey@degdigital.com>
2018-02-14 18:11:50 -0600

    updates

--- src.net/GlobalAssemblyVersion.cs
+++ src.net/GlobalAssemblyVersion.cs
@@ -5,7 +5,7 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.2.0")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
-[assembly: AssemblyInformationalVersion("0.0.2-")]
+[assembly: AssemblyVersion("0.0.3.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyInformationalVersion("0.0.3-")]
 
