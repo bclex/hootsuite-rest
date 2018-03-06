4c184280631f06e303b6ab027f22f458ee2b2514
Jim Nagorka <jnagorka@degdigital.com>
2018-03-06 16:54:39 -0600

    FromResults

--- src.net/Hootsuite-Rest/Domain/Interaction.cs
+++ src.net/Hootsuite-Rest/Domain/Interaction.cs
@@ -1,11 +1,18 @@
-﻿namespace Hootsuite.Domain
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Hootsuite.Domain
 {
     public class Interaction
     {
-        public long SocialNetworkId { get; set; }
-        public long TargetSocialNetworkId { get; set; }
+        public long FromSocialNetworkId { get; set; }
+        public long ToSocialNetworkId { get; set; }
         public string SocialNetworkType { get; set; }
-        public long MessageId { get; set; }
+        public long ExternalMessageId { get; set; }
         public string Type { get; set; }
+
+        public static Interaction[] FromResults(JObject result) => result != null ? JsonConvert.DeserializeObject<Interaction[]>(result["data"]["interactions"].ToString(), HootsuiteClient.JsonSerializerSettings) : null;
     }
 }
